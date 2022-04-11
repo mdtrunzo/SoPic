f1b2a917ad9ccf251cbc96d60c32c2f5f1794628
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-04-11 14:38:55 -0300

    lunes

--- src/components/funciones/ReplaceImg.jsx
+++ src/components/funciones/ReplaceImg.jsx
@@ -6,8 +6,8 @@ function ReplaceImg({close}) {
         return (
               <div className='replace-img'>
                 <Pulse>
-                    <p style={{paddingTop: '10px', color:'#785AFF'}}>Selecciona la imagen <br />que deseas reemplazar</p>
-                    <button>SUBIR FOTO</button>
+                    <p style={{paddingTop: '10px', color:'#785AFF'}}>Toca la imagen que <br /> deseas reemplazar</p>
+                    {/* <button>SUBIR FOTO</button> */}
                 </Pulse>
                   <CloseIcon className='close-panel' onClick={close}/>
               </div>

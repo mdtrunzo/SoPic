c7cdced5761b40c081593ced92fcafbbdbc36635
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2022-06-07 19:00:01 +0200

    terminar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import SaveIcon from '@mui/icons-material/Save';
 import ReplayIcon from '@mui/icons-material/Replay';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import Flip from 'react-reveal/Flip'
+import { Link } from 'react-router-dom'
 // import Avatar from './AvatarImage';
 
 function Header({login}) {
@@ -36,10 +37,13 @@ function Header({login}) {
                       <ReplayIcon />
                       <li>DESHACER</li>
                     </div>
+                    <Link to='https://testing.ribaslegales.com.ar/index.php/carrito/'>
                     <div className="iconos-menu">
                       <ShoppingBagIcon />
                       <li>TERMINAR</li>
                     </div>
+                    </Link>
+                   
                 </ul>
                 </nav>
                {/* <Avatar /> */}
---
+++ src/woocommerceAPI.js
@@ -0,0 +1,8 @@
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+
+const api = new WooCommerceRestApi({
+  url: "http://testing.ribaslegales.com.ar",
+  consumerKey: "ck_25f24e8f6d4bf26fa8d19fcd4600c6d3cdf62603",
+  consumerSecret: "cs_5cf22b8f67b8acc84683baf59d236ade9b467e09",
+  version: "wc/v3"
+});
